40a5de6ad3eca8db0026388ca8574f2df3a31fc9
Pham Le Huy Hoang <phamlehuyhoang@gmail.com>
2022-11-27 15:52:43 +0700

    add Tinh S(n)=x + x^2/2! + x^3/3! +...+x^n/n!

---
+++ 17.excercise/Tinh S(n)/Tinh S(n)/main.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+	int n,x;
+	printf("Enter n ");
+	scanf_s("%d", &n);
+	printf("\nEnter x ");
+	scanf_s("%d", &x);
+	float s=0;
+	int t = 1;
+	float result = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		t = t * i;
+		s =pow(x, i) / t ;
+		result = result + s;
+	}
+	 printf("\nResult is %.2f ",result);
+		
+	_getch();
+    return 0;
+}

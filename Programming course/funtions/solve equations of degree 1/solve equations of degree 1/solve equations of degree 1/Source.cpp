e9b175c8d39ef03bae81f8f64aa029cd1599d546
Pham Le Huy Hoang <phamlehuyhoang@gmail.com>
2022-12-04 22:41:58 +0700

    add solve equations of degree 1

---
+++ Programming course/funtions/solve equations of degree 1/solve equations of degree 1/solve equations of degree 1/Source.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+void Nhap(double &x)
+{
+	printf("\nenter a value ");
+	scanf_s("%lf", &x);
+}
+void  GPTB1(double a, double b)
+{
+	if (a == 0)
+	{
+		if (b == 0)
+		{
+			printf("\nptvsn");
+		}
+		else
+		{
+			printf("\nptvn");
+		}
+	}
+	else
+	{
+		double x = -b / a ;
+		printf("\nResult is %lf ", x);
+	}
+}
+int main()
+{
+	double a, b;
+	Nhap(a);
+	Nhap(b);
+	GPTB1(a, b);
+	_getch();
+	return 0;
+}

1235f10b725f5df0ba0d87811cfb4d599ea76bc9
Pham Le Huy Hoang <phamlehuyhoang@gmail.com>
2022-12-05 23:39:51 +0700

    add all of even divisor of a posible integer n

---
+++ Programming_technique_exercise/Chapter 1_Algorithm_flowchart/25.exercises/25.exercises/Source.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n;
+	int S = 0;
+	printf("\nEnter a posible integer n ");
+	scanf_s("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		if (n % i == 0 )
+		{
+			if (i % 2 == 0)
+			{
+				S = S + i;
+			}
+			
+		}
+	
+	}
+	printf("\nS=%d", S);
+
+	_getch();
+	return 0;
+}

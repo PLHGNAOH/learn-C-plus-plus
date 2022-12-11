8e8bef27afc93c1acefab18fe2e25b7ead7289de
Pham Le Huy Hoang <phamlehuyhoang@gmail.com>
2022-12-11 18:13:59 +0700

    add all

---
+++ Programming course/funtions/2. Repeat statement chapter exercises/2.a_Returns the island of its number/2.a_Returns the island of its number/Source.cpp
@@ -0,0 +1,31 @@
+﻿#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void nhap(int &n)
+{
+	printf("\nNhap n ");
+	scanf_s("%d", &n);
+
+}
+int TraVeSoDao(int n)
+{
+	int soluongchuso = log10((double)n);
+	int tong = 0;
+	while (n != 0)
+	{
+		tong += n % 10 * pow(10.0, soluongchuso--);
+		n /= 10; 
+	}
+	printf("\nkq %d", tong);
+	return tong;
+}
+int main()
+{
+	int n;
+	nhap(n);
+	int kq = TraVeSoDao(n);
+
+	_getch();
+	return 0;
+
+}
